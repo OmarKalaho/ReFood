ea05d16e63f785268afe94906b0d083f06c17272
Omar <omar.kalaho@gmail.com>
2023-02-22 22:27:51 +0200

    made hero page interactive

--- components/heroComp/body.js
+++ components/heroComp/body.js
@@ -4,9 +4,8 @@ const Body = () => {
   return (
     <p className={styles.h2}>
       Lets stop food waste and save the world! 
-      Refood is a Leftover Food Management, our goal is to connect as widly
-      and as effortlessly as possibe
-      food charities and food recycling factories, with any organization or person that
+      Our goal is to connect
+      Food Charities and Food Recycling Factories, with any Organization or Person that
       has leftover food that they want to dispose. <a className={styles.a}>Start Now!</a>
     </p>
   );
